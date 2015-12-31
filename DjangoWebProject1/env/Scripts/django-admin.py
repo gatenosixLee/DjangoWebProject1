d4f77ff8c21d0b11f2a1b4f8d5757bb4cddf0594
gatenosixLee <gatenosix@gmail.com>
2015-12-31 19:59:01 +0900

    test

---
+++ DjangoWebProject1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\GoogleDrive\PersonalWork\WebApp\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
